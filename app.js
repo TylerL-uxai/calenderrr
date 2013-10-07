9e55a2a8cc9c6e9445b9dd032de0be418604877d
Squ34k3rZ <Squ34k3rZ@gmail.com>
2013-10-07 00:26:15 +0000

    Commented out mongo from app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var mongo = require('mongodb');
+/**var mongo = require('mongodb');*/
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+/**var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
 mongo.Db.connect(mongoUri, function (err,db) {
     db.collection('mydocs', function(er, collection) {
@@ -18,7 +18,7 @@ mongo.Db.connect(mongoUri, function (err,db) {
 	});
     });
 });
-
+*/
 var app = express();
 
 // all environments
