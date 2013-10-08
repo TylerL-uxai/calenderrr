9bbf31305ab9c68177053317a32a3cbe6f7cac20
Squ34k3rZ <Squ34k3rZ@gmail.com>
2013-10-08 22:07:11 +0000

    Testing db code in app.js

--- app.js
+++ app.js
@@ -46,3 +46,23 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+//require node modules (see package.json)
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+//connect away
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+  if (err) throw err;
+  console.log("Connected to Database");
+
+  //simple json record
+    var document = {name:"David", title:"About MongoDB"};
+  
+    //insert record
+    db.collection('test').insert(document, function(err, records) {
+	if (err) throw err;
+	console.log("Record added as "+records[0]._id);
+	});
+});
