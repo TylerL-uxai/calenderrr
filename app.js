80ea181f6c940c7009fc56f8ee38b511d3e9bd71
Squ34k3rZ <Squ34k3rZ@gmail.com>
2013-10-15 20:55:04 +0000

    Trying to connect to db

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , EmployeeProvider = require('./employeeprovider').EmployeeProvider;
+var mongodb = require('mongodb');
+var logger = require('./logger.js');
+
+
 
 var app = express();
 
@@ -34,7 +38,42 @@ app.configure('development', function(){
 // var employeeProvider= process.env.MONGOLAB_URI || 'mongodb://localhost/test';
 
 
+// new stuff
 
+var MONGODB_URI = 'mongodb://localhost';
+var db;
+var coll;
+
+// Initialize connection once, reuse the database object 
+
+mongodb.MongoClient.connect(MONGODB_URI, { server: { logger: logger(MONGODB_URI) } }, function(err, database) {
+  if(err) throw err;
+ 
+  db = database;
+  coll = db.collection('test');
+
+  app.listen(3000);
+  console.log('Listening on port 3000');
+});
+
+app.get('/', function(req, res) { 
+  coll.find({}, function(err, docs) {
+    docs.each(function(err, doc) {
+      if(doc) {
+        res.write(JSON.stringify(doc) + "\n");
+      }
+      else {
+        res.end();
+      }
+    });
+  });
+});
+
+app.get('/post', function(req, res) {
+  coll.insert({ randomNumber: Math.random() }, function(err) {
+    res.end('Successful Insert!');
+  })
+});
 
 
 //Routes
