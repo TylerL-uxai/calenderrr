bd4c51bdf3d4ef340d3ca6f2b6cf0e468d8e3036
Squ34k3rZ <Squ34k3rZ@gmail.com>
2013-10-15 20:59:12 +0000

    Deleted app.js

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
-/**
- * Module dependencies.
- */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , EmployeeProvider = require('./employeeprovider').EmployeeProvider;
+var express = require('express');
 var mongodb = require('mongodb');
 var logger = require('./logger.js');
-
-
-
 var app = express();
-
-app.configure(function(){
-  app.set('port', process.env.PORT || 8080);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('view options', {layout: false});
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-//var employeeProvider= new EmployeeProvider('localhost', 27017);
-
-// var employeeProvider= process.env.MONGOLAB_URI || 'mongodb://localhost/test';
-
-
-// new stuff
 
 var MONGODB_URI = 'mongodb://localhost';
 var db;
@@ -74,33 +38,4 @@ app.get('/post', function(req, res) {
     res.end('Successful Insert!');
   })
 });
-
-
-//Routes
-
-app.get('/', function(req, res){
-  employeeProvider.findAll(function(error, emps){
-      res.render('index', {
-            title: 'Employees',
-            employees:emps
-        });
-  });
-});
-
-app.get('/employee/new', function(req, res) {
-    res.render('employee_new', {
-        title: 'New Employee'
-    });
-});
-
-//save new employee
-app.post('/employee/new', function(req, res){
-    employeeProvider.save({
-        title: req.param('title'),
-        name: req.param('name')
-    }, function( error, docs) {
-        res.redirect('/')
-    });
-});
-
-app.listen(8080);
+  
